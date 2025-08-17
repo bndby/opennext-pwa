3b94de20cae15b6e606f5e84d15384315d1c40cc
Bandarenka Yura <ydaren@gmail.com>
2025-08-17 23:24:52 +0300

    up

--- src/components/DeviceMotion/DeviceMotion.tsx
+++ src/components/DeviceMotion/DeviceMotion.tsx
@@ -1,27 +1,18 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { RadarChart } from '@mui/x-charts/RadarChart';
 
 export default function DeviceMotion() {
-    const [xData, setXData] = useState<number[]>([]);
-    const [yData, setYData] = useState<number[]>([]);
-    const [zData, setZData] = useState<number[]>([]);
+    const [xData, setXData] = useState<number>(0);
+    const [yData, setYData] = useState<number>(0);
+    const [zData, setZData] = useState<number>(0);
 
     useEffect(() => {
         const handleDeviceMotion = (event: DeviceMotionEvent) => {
-            setXData((prev) => {
-                const newData = [...prev, event.acceleration?.x || 0];
-                return newData.length > 50 ? newData.slice(-50) : newData;
-            });
-            setYData((prev) => {
-                const newData = [...prev, event.acceleration?.y || 0];
-                return newData.length > 50 ? newData.slice(-50) : newData;
-            });
-            setZData((prev) => {
-                const newData = [...prev, event.acceleration?.z || 0];
-                return newData.length > 50 ? newData.slice(-50) : newData;
-            });
+            setXData(event.acceleration?.x ?? 0);
+            setYData(event.acceleration?.y ?? 0);
+            setZData(event.acceleration?.z ?? 0);
         };
 
         window.addEventListener('devicemotion', handleDeviceMotion);
@@ -33,26 +24,13 @@ export default function DeviceMotion() {
 
     return (
         <div>
-            <h2>X-координата</h2>
-            <LineChart
+            <RadarChart
                 height={300}
-                series={[
-                    {
-                        label: 'X',
-                        data: xData,
-                        showMark: false,
-                    },
-                    {
-                        label: 'Y',
-                        data: yData,
-                        showMark: false,
-                    },
-                    {
-                        label: 'Z',
-                        data: zData,
-                        showMark: false,
-                    },
-                ]}
+                series={[{ label: 'Acceleration', data: [xData, yData, zData] }]}
+                radar={{
+                    max: 120,
+                    metrics: ['X', 'Y', 'Z'],
+                }}
             />
         </div>
     );
